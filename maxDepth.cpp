96a067d30e6cb95e6d7847d62b4a9bf2fee6550f
yyelaine <yyelaine@foxmail.com>
2017-01-09 23:01:08 +0800

    二叉树的最大深度

---
+++ maxDepth.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer
+     */
+    int maxDepth(TreeNode *root) {
+        // write your code here
+        if(!root)
+        return 0;
+        int leftDepth = maxDepth(root->left)+1;
+        int rightDepth = maxDepth(root->right)+1;
+        return max(leftDepth,rightDepth);
+    }
+};
