3027e3bbc6536bda1f335133df02ccee2a98aa67
yyelaine <yyelaine@foxmail.com>
2017-01-16 23:05:33 +0800

    翻转二叉树

---
+++ invertBinaryTree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+    /**
+     * @param root: a TreeNode, the root of the binary tree
+     * @return: nothing
+     */
+    void invertBinaryTree(TreeNode *root) {
+        // write your code here
+        if(root==NULL)
+		return NULL;
+	TreeNode * ptmpNode = root->left;
+	root->left = invertTree(root->right);
+	root->right = invertTree(ptmpNode);
+	return root;
+   }
+};
