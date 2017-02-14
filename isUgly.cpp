fe94a6f0c4acacdedd947d92f3619367a5058a9e
yyelaine <yyelaine@foxmail.com>
2017-02-14 22:50:54 +0800

    丑数

---
+++ isUgly.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    /**
+     * @param num an integer
+     * @return true if num is an ugly number or false
+     */
+    bool isUgly(int num) {
+        // Write your code here
+        if(num<=0)
+            return false;
+            else
+            {
+            while(num%2==0)
+            num/=2;
+            while(num%3==0)
+            num/=3;
+            while(num%5==0)
+            num/=5;
+            return (num==1)?true:false;
+            }
+    }
+};
